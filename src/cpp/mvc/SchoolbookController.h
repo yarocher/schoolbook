ac4cbb01ec60ca1413994e2aecfd1298818acf41
Yaroslav <yaro01ir@gmail.com>
2021-05-12 00:02:13 +0300

    took out View

--- src/cpp/mvc/SchoolbookController.h
+++ src/cpp/mvc/SchoolbookController.h
@@ -1,23 +1,24 @@
 #include <iostream>
 #include "DAO.h"
+#include "SchoolbookView.h"
 
 using namespace std;
 
 class SchoolbookController {
+	SchoolbookView view;
 	public:
 		void processInput () {
 			string input;
-			cout << "Please, enter number of section:\n";
+			view.printInputRequest();
 			cin >> input;
 			try {
 				int number = stoi(input);
 				DAO dao;
 				Section section = dao.getSection(1);	
-				cout << section.toString();
+				view.printSection(section);
 			}
 			catch (const exception& e) {
-				cout << "Something went wrong while trying to get section\n";
-				cerr << e.what();
+				view.printException("Something went wrong while trying to get section\n", e);
 			}
 		}
 };
--- src/cpp/mvc/SchoolbookView.h
+++ src/cpp/mvc/SchoolbookView.h
@@ -3,5 +3,15 @@
 using namespace std;
 
 class SchoolbookView {
-
+	public:
+		void printSection (Section section) {
+			cout << section.toString();
+		}
+		void printInputRequest () {
+			cout << "Please, enter number of section:\n";
+		}
+		void printException (string msg, exception e) {
+			cout << msg;
+			cerr << e.what();
+		}
 };
