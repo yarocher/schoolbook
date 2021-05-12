d734cd5c3c4d2cedf61819246e2e6751d2cbfc48
Yaroslav <yaro01ir@gmail.com>
2021-05-12 11:00:20 +0300

    rename linux bin

--- bin/compile.sh
+++ bin/compile.sh
@@ -2,6 +2,6 @@
 
 #should be executed from root of the project
 rm out/*
-g++ src/cpp/Main.cpp -o out/main -I src/cpp/data -I src/cpp/entity -I src/cpp/mvc -I lib/rapidxml-1.13 
+g++ src/cpp/Main.cpp -o out/main.o -I src/cpp/data -I src/cpp/entity -I src/cpp/mvc -I lib/rapidxml-1.13 
 
 
